ee46b8fcca01c206ebf9dcba1f3ada229184ec2b
RimonChakma <rimon.chakma880@gmail.com>
2025-03-22 22:40:08 +0600

    add product create screen

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:crud_api_project/utility/product_create_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProductViewScreen(),
+    );
+  }
+}
---
+++ lib/utility/product_create_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProductViewScreen extends StatefulWidget {
+  const ProductViewScreen({super.key});
+
+  @override
+  State<ProductViewScreen> createState() => _ProductViewScreenState();
+}
+
+class _ProductViewScreenState extends State<ProductViewScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Page"),
+        centerTitle: true,
+      ),
+
+
+    );
+  }
+}
