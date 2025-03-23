002248be93c98699eb574e1fe1d6e95d37bf62ed
RimonChakma <rimon.chakma880@gmail.com>
2025-03-23 19:38:24 +0600

    add dropdownButton

--- lib/utility/product_create_screen.dart
+++ lib/utility/product_create_screen.dart
@@ -22,15 +22,32 @@ class _ProductViewScreenState extends State<ProductViewScreen> {
     body: Padding(padding: EdgeInsets.all(10),child: Column(
     children: [
     TextFormField(decoration: inputDecoratorStyle("productname")),
+
     SizedBox(height: 10,),
     TextFormField(decoration: inputDecoratorStyle("productcode")),
+
     SizedBox(height: 10,),
     TextFormField(decoration: inputDecoratorStyle("productimage")),
+
     SizedBox(height: 10,),
     TextFormField(decoration: inputDecoratorStyle("unitprice")),
+
     SizedBox(height: 10,),
     TextFormField(decoration: inputDecoratorStyle("totalprice")),
 
+      SizedBox(height: 10,),
+
+      DropdownButton(items: [
+    DropdownMenuItem(child: Text("select item"),value: "",),
+    DropdownMenuItem(child: Text("1st"),value: "1st",),
+    DropdownMenuItem(child: Text("2nd"),value: "2nd",),
+    DropdownMenuItem(child: Text("3rd"),value: "3rd",),
+    DropdownMenuItem(child: Text("4th"),value: "4th",),
+      ], onChanged: (value) {
+
+      },
+      isExpanded: true,
+      )
 
     ],
     )));
